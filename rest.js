58f0f420663f0ce38431d90c9924a68d28314ff8
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-12-04 11:49:53 +0100

    finished

--- rest.js
+++ rest.js
@@ -1,19 +1,52 @@
 // ************************ Exempel 1 ************************//
 // Samla ihop parametrar till en funktion. Sparas i en array.
 
+// const add = (...args) => {
+//     let sum = 0;
+//     for(let i = 0; i < args.length; i++) {
+//         sum += args[i];
+//     }
+//     return sum;
+// }
+
+// console.log(add(1,2,3,4,5,7,8,9))
 
 
 
-
-
-// ************************ Exempel 2 ************************//
+// // ************************ Exempel 2 ************************//
 // Samla ihop resten av parametrarna till en funktion. Sparas i en array.
+
+const add = (x, ...args) => {
+    let sum = 0;
+    for(let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum * x;
+}
+
+console.log(add(100, 1,2,3,4,5,7,8,9))
 
 
 
 // ************************ Exempel 3 ************************//
 // Rest i objekt
+const person = {
+    name: 'Sandra',
+    age: 30,
+    city: 'Stockholm',
+    country: 'Sweden',
+    email: 'sandra@larsson.com'
+}
+
+const {name, ...details} = person;
+
+console.log(name);
+console.log(details);
+
 
 
 // ************************ Exempel 4 ************************//
 // Rest i array
+const numbers = [1, 2, 3, 4, 5];
+
+const [first, ...rest] = numbers;
--- spread.js
+++ spread.js
@@ -4,6 +4,12 @@
 // Spread "sprider" värden från en array till en ny array.
 
 
+  // const numbers = [1, 2, 3, 4, 5];
+
+  // const newNumbers = [...numbers, 6, 7, 8, 9, 10]
+
+  // console.log(newNumbers);
+
 
 // ************************ Exempel 2 ************************//
 // Spread kopierar hela objektet.
@@ -14,40 +20,57 @@ const person = {
 }
 
 
-console.log(newPerson);
+const newPerson = {
+      ...person,
+      city: "Stockholm"
+}
 
-// eller array
-const original = [1, 2, 3];
-
-
-
-console.log(original); // Output: [1, 2, 3] (Oförändrad)
-console.log(copy);     // Output: [1, 2, 3, 4]
-
-
-
+//console.log(newPerson);
 
 // ************************ Exempel 3 ************************//
-// Spread kopierar värden från en array till en funktion.
+  // Kopiera en array eller objekt
 
 
-const numbers3 = [1, 2, 3];
+const original = [1, 2, 3];
 
+const copy = [...original];  // Shallow copy
+
+console.log("original", original); // Output: [1, 2, 3] (Oförändrad)
+console.log("copy", copy);     // Output: [1, 2, 3, 4]
 
 
 
 // ************************ Exempel 4 ************************//
+// Spread kopierar värden från en array till en funktion.
+
+
+const numbers3 = [1, 2, 3];
+const add = (a, b, c) => a + b + c;
+
+const result = add(...numbers3);
+console.log(result);
+
+
+
+
+// ************************ Exempel 5 ************************//
 // Med spread kan man merga flera arrayer eller objekt. Istället för att använda concat.
 
 const numbers4 = [1, 2, 3];
 const numbers5 = [4, 5, 6];
 const numbers6 = [7, 8, 9];
 
+const mergedNumbers = [...numbers4, ...numbers5, ...numbers6];
 
 console.log(mergedNumbers);
 
-// ************************ Exempel 5 ************************//
-// Använd spread istället för slice.
+// ************************ Exempel 6 ************************//
+// Använd .. rest istället för slice.
 
-  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const sourceWithoutFirstTwo = removeFirstTwo(source);
+   const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+   const [, , ...shorterList] = source;
+   console.log(shorterList);
+
+  //const sourceWithoutFirstTwoSlice = source.slice(2);
+  //console.log(sourceWithoutFirstTwoSlice);
